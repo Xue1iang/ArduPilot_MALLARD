086c1b538be7d23b65fd612329f847bbe2ea9f4c
Xue1iang <77399327+Xue1iang@users.noreply.github.com>
2021-07-19 10:53:34 +0100

    Add mallard's thruster allocation config

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -183,10 +183,10 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
         break;
 
     case SUB_FRAME_CUSTOM:
-        add_motor_raw_6dof(AP_MOTORS_MOT_1,     1,              0,              0,           0,                  0,              0,           1);
-        add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              1,              0,          0,                  0,              0,          2);
-        add_motor_raw_6dof(AP_MOTORS_MOT_3,     0,              0,              1,          0,                  0,               0,           3);
-        add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,              0,           1,                  0,               0,          4);
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,     0,              0,              -1.0f,           0,                  1.0f,               1.0f,           1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              0,              1.0f,           0,                  1.0f,               -1.0f,           2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,     0,              0,              1.0f,           0,                  -1.0f,               1.0f,           3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,              -1.0f,           0,                  -1.0f,               -1.0f,           4);
         break;
 
     case SUB_FRAME_SIMPLEROV_3:
